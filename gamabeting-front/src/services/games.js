76110a9ac38c58a7bb41708b6f66bcba7bea8221
Christopher S. Rozario <christopherscotini@gmail.com>
2019-03-12 11:32:17 -0300

    add url prod betadmin

--- gamabeting-front/src/services/games.js
+++ gamabeting-front/src/services/games.js
@@ -1,7 +1,11 @@
 import axiosInstance from 'axios';
 
-const baseUrl = 'http://3.89.202.205:8081/gamabetting';
+// PROD
+// const postUrl = 'http://3.89.202.205:8180/betwin-admin';
+
+// DEV
 const postUrl = 'http://3.89.202.205:8080/betwin-admin';
+const baseUrl = 'http://3.89.202.205:8280/gamabetting';
 
 export const getEvents = async () => {
     const response = await axiosInstance.get(`${baseUrl}/v1/event-types/1/events`);
